148daf1b825ccf552b2fa4dd0ffe1b5aee4a6b4e
kristian334 <kdzidz00@fesb.hr>
2023-03-13 16:06:02 +0100

    NOVI console

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace pis1mz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+
         }
     }
 }
